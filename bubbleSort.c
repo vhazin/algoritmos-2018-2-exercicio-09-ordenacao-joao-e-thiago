bbf2c3b4e7d6e62dcda24b860f39b23b4ff6928b
Thiago <tjcm@cesar.school>
2018-11-05 09:21:49 -0300

    funções atualizadas com timer

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 
 
 
 void printSorted(int * arr, int size);
 
 int main(){
-
+    
+    clock_t start, end;
+    float cpu_time_used;
+    
     int size;
     printf("\nDigite a quantidade de elementos>> ");
     scanf("%d",&size);
@@ -15,8 +19,14 @@ int main(){
         printf(">>");
         scanf("%d",&data[i]);
     }
-
+    
+    start = clock();
     printSorted(data,size);
+    end = clock();
+    cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("\n o tempo necessario para função foi %f \n", cpu_time_used);
+    
 
 
     return 0;
--- heapSort.c
+++ heapSort.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 
 
 void printSorted(int * arr, int size);
 
 int main(){
+    
+    clock_t start, end;
+    float cpu_time_used;
 
     int size;
     printf("\nDigite a quantidade de elementos>> ");
@@ -15,8 +19,16 @@ int main(){
         printf(">>");
         scanf("%d",&data[i]);
     }
+    
+    start = clock();
 
     printSorted(data,size);
+    
+    end = clock();
+    
+    cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("\n o tempo necessario para rodar a função foi %f \n", cpu_time_used);
 
 
     return 0;
