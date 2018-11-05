452f6191f47084dd060738dde6169d31cf101e65
Thiago <tjcm@cesar.school>
2018-11-05 09:17:08 -0300

    funções com timer

--- selectionSort.c
+++ selectionSort.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <time.h>
 
 
 
 void printSorted(int * arr, int size);
 
 int main(){
-
+    clock_t start, end;
+    float cpu_time_used;
+    
     int size;
     printf("\nDigite a quantidade de elementos>> ");
     scanf("%d",&size);
@@ -16,7 +19,12 @@ int main(){
         scanf("%d",&data[i]);
     }
 
+    start = clock();
     printSorted(data,size);
+    end = clock();
+    cpu_time_used = ((float) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("\n o tempo gasto para função foi %f \n",cpu_time_used);
 
 
     return 0;
